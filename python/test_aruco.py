359ad633237be465076c3445ae898d969e50970d
Miquel Massot <miquel.massot@gmail.com>
2018-06-06 11:45:45 +0200

    port code to python and add aruco detection

---
+++ python/test_aruco.py
@@ -0,0 +1,28 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+#dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_5X5_1000)
+dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
+#dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_ARUCO_ORIGINAL)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    res = cv2.aruco.detectMarkers(gray,dictionary)
+#   print(res[0],res[1],len(res[2]))
+
+    if len(res[0]) > 0:
+        cv2.aruco.drawDetectedMarkers(gray,res[0],res[1])
+    # Display the resulting frame
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
