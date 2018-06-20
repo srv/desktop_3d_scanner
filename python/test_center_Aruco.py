527b646cd29aece40e409b2627981105c7e01561
Marta Pons <mpn26141@gmail.com>
2018-06-20 13:43:30 +0200

    calib Table
    With this code I find the plane and the center of the Table

--- python/test_center_Aruco.py
+++ python/test_center_Aruco.py
@@ -6,7 +6,13 @@ Created on Mon Jun 18 16:04:54 2018
 """
 import numpy as np
 import cv2
-import time
+
+def fitPlane(norm,point):
+    A=norm[0]
+    B=norm[1]
+    C=norm[2]
+    D=-np.sum(norm*point)
+    return(A,B,C,D)
 
 camera_matrix = np.load("cameraCalibMatrix.npy")
 dist_coeffs = np.load("cameraCalibDistCoeff.npy")
@@ -32,19 +38,42 @@ while(True):
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         
         corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(gray,dictionary)
+        
+        tvecs5=np.array([[[0,0,0]]])
+        tvecs42=np.array([[[0,0,0]]])
+        tvecs27=np.array([[[0,0,0]]])
+        tvecs18=np.array([[[0,0,0]]])
+        
+        if ids is not None:
+            cv2.aruco.drawDetectedMarkers(gray,corners,ids)
+            i=0
+            while(i<len(ids)):
+                rvecs,tvecs,_objPoints  = cv2.aruco.estimatePoseSingleMarkers(corners[i],marker_length,camera_matrix,dist_coeffs)
+                cv2.aruco.drawAxis(gray,camera_matrix,dist_coeffs,rvecs,tvecs,0.05)
+                if (ids[i]==5):
+                    R,_=cv2.Rodrigues(rvecs)
+                    t = np.array([-0.09,0,0])
+                    tvecs5=np.matmul(R,t.T)+tvecs
+                if (ids[i]==42):
+                    R,_=cv2.Rodrigues(rvecs)
+                    t = np.array([+0.09,0,0])
+                    tvecs42=np.matmul(R,t.T)+tvecs
+                if (ids[i]==27):
+                    R,_=cv2.Rodrigues(rvecs)
+                    t = np.array([0,-0.09,0])
+                    tvecs27=np.matmul(R,t.T)+tvecs
+                if (ids[i]==18):
+                    R,_=cv2.Rodrigues(rvecs)
+                    t = np.array([0,+0.09,0])
+                    tvecs18=np.matmul(R,t.T)+tvecs
 
-        if len(ids) > 0:
-                cv2.aruco.drawDetectedMarkers(gray,corners,ids)
-
-                i=0
-                while(i<len(ids)):
-                    rvecs,tvecs,_objPoints  = cv2.aruco.estimatePoseSingleMarkers(corners[i],marker_length,camera_matrix,dist_coeffs)
-                    cv2.aruco.drawAxis(gray,camera_matrix,dist_coeffs,rvecs,tvecs,0.05)
-                    i = i+1
-#                    planeTable = rvecs 
-#                    planeTable_D = np.sum(rvecs*tvecs)
-#                    print(planeTable)
-#                    print(planeTable_D)
+                i=i+1
+                  
+            tvecs = (tvecs5+tvecs42+tvecs27+tvecs18)/len(ids) #si té tvecs més petit té més prioritat
+            norm = R[0:3,2]
+            planeTable=fitPlane(norm,tvecs[0,0,0])
+            cv2.aruco.drawAxis(gray,camera_matrix,dist_coeffs,rvecs,tvecs,0.05)
+            
         # Display the resulting frame
         cv2.imshow('frame',gray)
         
